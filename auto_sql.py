e8222655dbcf6ba5e21e15e77947f21d503a904c
Brett Vanderwerff <brett.vanderwerff@gmail.com>
2018-09-17 18:43:24 -0700

    Update auto_sql.py

--- auto_sql.py
+++ auto_sql.py
@@ -24,31 +24,36 @@ def count_file_lines(file):
 
 def get_chunk_count(file):
     core_count = multiprocessing.cpu_count()
-    if get_file_size(file) < get_mem():
+    file_size = get_file_size(file)
+    memory = get_mem()
+    if file_size < memory:
         return core_count
     else:
-        return (get_file_size(file)/ get_mem()) * core_count
+        return (file_size/ memory) * core_count
 
 def write_sql(df):
     con = sqlite3.connect('test.db')
-    df.to_sql("df", con, if_exists='append') # might save header for each df
+    df.to_sql("df", con, if_exists='append', index=False) # might save header for each df
     con.close()
 
 
-def read_csv(file):
+def read_csv(file, chunk_count, line_count):
     core_count = multiprocessing.cpu_count()
-    nrows = int(count_file_lines(file) / (get_chunk_count(file)))
+    nrows = int(line_count / chunk_count)
     skiprows = 0
     for core in range(core_count):
         if core == 0:
-            df = pd.read_csv(file, sep="\t", nrows=nrows, header=None)
+            df = pd.read_csv(file, sep=",", nrows=nrows)
             skiprows += nrows
+            names = df.columns.values
         else:
-            df = pd.read_csv(file, sep="\t", skiprows=skiprows, nrows=nrows, header=None) #4329313 total lines nrow 697492
+            df = pd.read_csv(file, sep=",", skiprows=skiprows, nrows=nrows, header=None, names=names) #4329313 total lines nrow 697492
             skiprows += nrows
 
         write_sql(df)
 
 
 if __name__ == "__main__":
-    read_csv(file="part-ii-dependency-paths-gene-gene-sorted-with-themes.txt")
+    chunk_count = get_chunk_count(file="surveys.csv")
+    line_count = count_file_lines(file="surveys.csv")
+    read_csv(file="surveys.csv", chunk_count=chunk_count, line_count=line_count)
