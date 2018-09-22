664b6a4b7a7dac9a75f64294ad9a9572d82cb920
Brett.Vanderwerff <vande060@gmail.com>
2018-09-21 22:33:23 -0700

    added OOP

--- auto_sql.py
+++ auto_sql.py
@@ -4,81 +4,100 @@ from psutil import virtual_memory
 import sqlite3
 import multiprocessing
 import math
-import numpy as np
 pd.set_option('display.max_columns', 1)
 
-def get_mem(buffer=.75):
-    print("Calculating available RAM...")
-    mem = virtual_memory().free * buffer
-    str_mem = str(round(mem / 10 ** 9, 2))
-    print(str_mem + " GB available RAM detected\n")
-    return mem
 
-def get_file_size(file):
-    print("Calculating file size...")
-    file_size = os.path.getsize(file)
-    str_file_size = str(round(file_size/10**9, 2))
-    print(str_file_size + " GB file detected\n")
-    return os.path.getsize(file)
+class AutoSql():
+    def __init__(self, file, db_name, sep, buffer=.75):
+        self.skiprows = 0
+        self.file = file
+        self.db_name = db_name
+        self.sep = sep
+        self.buffer = buffer
 
-def count_file_lines(file):
-    print("Counting file rows...")
-    with open(file) as read_obj:
-        read_obj.readline() #Read a line to skip the header
-        line_count = 0
-        for line in read_obj:
-            line_count += 1
-            if line_count%100000 == 0:
-                print(str(line_count) + ' lines read')
-    print('\n' + str(line_count) + " lines found")
-    return line_count
+    def get_mem(self):
+        print("Calculating available RAM...")
+        memory = virtual_memory().free * self.buffer
+        str_mem = str(round(memory / 10 ** 9, 2))
+        print(str_mem + " GB available RAM detected\n")
+        return memory
 
-def get_chunk_count(file):
-    core_count = multiprocessing.cpu_count()
-    file_size = get_file_size(file)
-    memory = get_mem()
-    if file_size < memory:
-        return core_count
-    else:
-        return math.ceil((file_size / memory) * core_count)
+    def get_file_size(self):
+        print("Calculating file size...")
+        file_size = os.path.getsize(self.file)
+        str_file_size = str(round(file_size/10**9, 2))
+        print(str_file_size + " GB file detected\n")
+        return os.path.getsize(self.file)
 
-def write_sql(file, df, db_name):
-    print('Writing chunk to disk')
-    table_name = os.path.basename(file).split(sep='.')[0]
-    con = sqlite3.connect('{}.db'.format(db_name))
-    df.to_sql(table_name , con, if_exists='append', index=False)
-    con.close()
+    def count_file_lines(self):
+        print("Counting file rows...")
+        with open(self.file) as read_obj:
+            read_obj.readline() #Read a line to skip the header
+            line_count = 0
+            for line in read_obj:
+                line_count += 1
+                if line_count%100000 == 0:
+                    print(str(line_count) + ' lines read')
+        print('\n' + str(line_count) + " lines found")
+        return line_count
 
-def get_line_list(line_count, chunk_count):
-    line_list = []
-    for i in range(1, chunk_count):
-        line_list.append(line_count // chunk_count)
-    line_list.append(line_count // chunk_count + line_count % chunk_count)
-    return line_list
-
-
-def read_csv(file, line_list, db_name, sep):
-    mp_list = np.array_split(line_list, 4)
-    print(mp_list)
-    skiprows = 0
-    for counter, line_count in enumerate(line_list):
-        print(line_list)
-        if counter == 0:
-            df = pd.read_csv(file, sep=sep, nrows=line_count)
-            skiprows += line_count
-            names = df.columns.values
+    def get_chunk_count(self):
+        core_count = multiprocessing.cpu_count()
+        file_size = self.get_file_size()
+        #memory = get_mem() uncomment for production and delete below line
+        memory = 1011723776
+        if file_size < memory:
+            return core_count
         else:
-            df = pd.read_csv(file, sep=sep, skiprows=skiprows, nrows=line_count, header=None, names=names)
-            skiprows += line_count
+            return math.ceil((file_size / memory) * core_count)
 
-        write_sql(file=file, df=df, db_name=db_name)
+    def write_sql(self, df):
+        print('Writing chunk to disk')
+        table_name = os.path.basename(self.file).split(sep='.')[0]
+        con = sqlite3.connect('{}.db'.format(self.db_name))
+        df.to_sql(table_name , con, if_exists='append', index=False)
+        con.close()
 
-def run(file, db_name, sep):
-    chunk_count = get_chunk_count(file=file)
-    line_count = count_file_lines(file=file)
-    line_list = get_line_list(line_count=line_count, chunk_count=chunk_count)
-    read_csv(file=file, line_list=line_list, db_name=db_name, sep=sep)
+    def get_line_list(self, line_count, chunk_count):
+        line_list = []
+        for i in range(1, chunk_count):
+            line_list.append(line_count // chunk_count)
+        line_list.append(line_count // chunk_count + line_count % chunk_count)
+        return line_list
+
+
+    def read_csv(self, inner_line_list):
+        processess = []
+        for counter, line_count in enumerate(inner_line_list):
+            if counter == 0:
+                print('Reading chunk to memory')
+                df = pd.read_csv(self.file, sep=self.sep, nrows=line_count)
+                self.skiprows += line_count
+                names = df.columns.values
+            else:
+                print('Reading chunk to memory')
+                df = pd.read_csv(self.file, sep=self.sep, skiprows=self.skiprows, nrows=line_count, header=None, names=names)
+                self.skiprows += line_count
+
+            self.write_sql(df=df)
+
+
+    def mp_handler(self, line_list):
+        core_count = multiprocessing.cpu_count()
+        mp_list = [line_list[i:i + core_count] for i in range(0, len(line_list), core_count)]
+        for inner_line_list in mp_list:
+            self.read_csv(inner_line_list=inner_line_list)
+
+
+    def run(self):
+        chunk_count = self.get_chunk_count()
+        line_count = self.count_file_lines()
+        line_list = self.get_line_list(line_count=line_count, chunk_count=chunk_count)
+        self.mp_handler(line_list=line_list)
 
 
 if __name__ == "__main__":
-    run(file='test.txt', db_name='database', sep='\t')
+    my_object = AutoSql(file='test.txt', db_name='database', sep='\t')
+    print(my_object.skiprows)
+    my_object.run()
+    print(my_object.skiprows)
